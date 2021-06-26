85ba3461be5d81de8f3ac6e9f622707bfe90e21d
Marcello Tavano <chelotavano.udp@gmail.com>
2021-06-25 21:05:46 -0400

    Update bot folder

--- bot/bot.go
+++ bot/bot.go
@@ -12,13 +12,14 @@ type Bot struct {
 	referencePrice float64 // from local exchange
 	buda           BudaClient
 
-	config Config
+	config *Config
 
 	// ticker use only
 	now               TimeWrapper
 	lastRequestMadeAt time.Time
 }
 
+// Config is a bot config type
 type Config struct {
 	TargetVolume     float64 // in local currency
 	RateToAction     float64 // the fall that we are looking for
@@ -26,21 +27,59 @@ type Config struct {
 	Currency         string
 }
 
+func New(config *Config, budaClient BudaClient) *Bot {
+	b := &Bot{
+		buda:   budaClient,
+		config: config,
+	}
+
+	b.sm = statemachine.New(
+		&statemachine.Event{Name: eventGetTicker},
+		map[string]statemachine.State{
+			eventGetTicker: {
+				EventHandler: b.GetTickerHandler,
+				Destination:  []string{eventGetTicker, eventCalculate},
+			},
+			eventCalculate: {
+				EventHandler: b.CalculateHandler,
+				Destination:  []string{eventCalculate, eventGetTicker, eventCreateOrder},
+			},
+			eventCreateOrder: {
+				EventHandler: b.BuyCryptoHandler,
+				Destination:  []string{eventFinish},
+			},
+			eventFinish: {
+				EventHandler: b.FinishHandler,
+				Destination:  []string{statemachine.EventAbort},
+			},
+		},
+		nil,
+	)
+
+	return b
+}
+
+func (b *Bot) Start() {
+	b.sm.Run()
+}
+
+// BudaClient Definition of buda client interface
 type BudaClient interface {
 	GetTicker(pair string) (*buda.Ticker, error)
 	CreateOrder(pair string, req *buda.CreateOrderRequest) (*buda.Order, error)
 	GetOrder(id string) (*buda.Order, error)
 }
 
+// TimeWrapper is the type alias for the .Now() function
 type TimeWrapper func() time.Time
 
+// payload passed through events
 type eventPayload struct {
 	ticker   *buda.Ticker
 	buyPrice float64
 }
 
+// parseData parses the generic map into a eventPayload type
 func (b *Bot) parseData(data map[string]interface{}) *eventPayload {
 	return data["payload"].(*eventPayload)
 }
-
-var bitSize = 64
--- bot/calculate_handler.go
+++ bot/calculate_handler.go
@@ -11,12 +11,19 @@ const (
 	eventCalculate = "event-calculate"
 )
 
+var bitSize = 64
+
 func (b *Bot) CalculateHandler(e *statemachine.Event) (*statemachine.Event, error) {
 	payload := b.parseData(e.Data)
 
 	ask, err := strconv.ParseFloat(payload.ticker.MinAsk[0], bitSize)
 	if err != nil {
 		return nil, errors.Wrap(err, "bot: Bot CalculateHandler strconv.ParseFloat error")
+	}
+
+	// if is the first execution
+	if b.referencePrice == 0 {
+		b.referencePrice = ask
 	}
 
 	if ask <= (b.referencePrice*1 + (b.config.RateToAction / 100)) {
